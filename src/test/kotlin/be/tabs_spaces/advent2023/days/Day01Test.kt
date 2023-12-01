40d71d3aa5c40eaa4a7ca780cb9be58dedca7fd2
Jan Van Ryckeghem <jan@tabs-spaces.be>
2023-12-01 08:13:06 +0100

    Day 01

---
+++ src/test/kotlin/be/tabs_spaces/advent2023/days/Day01Test.kt
@@ -0,0 +1,19 @@
+package be.tabs_spaces.advent2023.days
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day01Test {
+    private val day01 = Day01()
+
+    @Test
+    fun partOne() {
+        assertThat(day01.partOne()).isEqualTo(142)
+    }
+
+    @Test
+    fun partTwo() {
+        assertThat(day01.partTwo()).isEqualTo(142)
+    }
+
+}
