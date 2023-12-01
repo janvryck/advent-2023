30deb39b214aae2d7440c3b22e5b6aba88a63c2e
Jan Van Ryckeghem <jan@tabs-spaces.be>
2023-12-01 12:57:12 +0100

    Day 01 - refactor - shorten digits setup

--- src/main/kotlin/be/tabs_spaces/advent2023/days/Day01.kt
+++ src/main/kotlin/be/tabs_spaces/advent2023/days/Day01.kt
@@ -2,17 +2,7 @@ package be.tabs_spaces.advent2023.days
 
 class Day01 : Day(1) {
 
-    private val digits = mapOf(
-        "1" to 1,
-        "2" to 2,
-        "3" to 3,
-        "4" to 4,
-        "5" to 5,
-        "6" to 6,
-        "7" to 7,
-        "8" to 8,
-        "9" to 9,
-    )
+    private val digits = (1..9).associateBy { "$it" }
     private val numericals = digits + mapOf(
         "one" to 1,
         "two" to 2,
